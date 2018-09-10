43d649f0f325fcb4989c07ef5814c0de639211b2
Emmanuel Castillo <emcastillo@csumb.edu>
2018-09-10 08:05:45 +0000

    added css precedence testing

---
+++ practice/css/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>CSS Examples and Precedent</title>
+        <link href="./sheet1.css" rel="stylesheet">
+    </head>
+    <body>
+        <style>
+            /* All paragraphs in the document */
+            p{
+                margin: 10px;
+            }
+        </style>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eu mi bibendum neque egestas congue quisque. Felis donec et odio pellentesque. Id velit ut tortor pretium viverra suspendisse potenti nullam ac. In hac habitasse platea dictumst. Elit at imperdiet dui accumsan sit amet nulla facilisi morbi. Ultricies mi quis hendrerit dolor magna eget. Ultricies lacus sed turpis tincidunt id aliquet. Convallis tellus id interdum velit laoreet id donec ultrices. Velit sed ullamcorper morbi tincidunt ornare massa eget. Aliquet porttitor lacus luctus accumsan tortor posuere ac. Scelerisque viverra mauris in aliquam sem.
+        </p>
+        <p class="highlighted">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eu mi bibendum neque egestas congue quisque. Felis donec et odio pellentesque. Id velit ut tortor pretium viverra suspendisse potenti nullam ac. In hac habitasse platea dictumst. Elit at imperdiet dui accumsan sit amet nulla facilisi morbi. Ultricies mi quis hendrerit dolor magna eget. Ultricies lacus sed turpis tincidunt id aliquet. Convallis tellus id interdum velit laoreet id donec ultrices. Velit sed ullamcorper morbi tincidunt ornare massa eget. Aliquet porttitor lacus luctus accumsan tortor posuere ac. Scelerisque viverra mauris in aliquam sem.
+        </p>
+        <p id="first">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eu mi bibendum neque egestas congue quisque. Felis donec et odio pellentesque. Id velit ut tortor pretium viverra suspendisse potenti nullam ac. In hac habitasse platea dictumst. Elit at imperdiet dui accumsan sit amet nulla facilisi morbi. Ultricies mi quis hendrerit dolor magna eget. Ultricies lacus sed turpis tincidunt id aliquet. Convallis tellus id interdum velit laoreet id donec ultrices. Velit sed ullamcorper morbi tincidunt ornare massa eget. Aliquet porttitor lacus luctus accumsan tortor posuere ac. Scelerisque viverra mauris in aliquam sem.
+        </p>
+        
+        <img src="https://scontent-sea1-1.cdninstagram.com/vp/e998579c21f1dcd573358fdcabb71dfb/5B9E2795/t51.2885-15/s480x480/e35/c180.0.719.719/32150375_181306192534901_8445781964694224896_n.jpg"style="width:300px;">
+        <style>
+            img{
+                width:400px;
+            }
+        </style>
+        
+        
+    </body>
+</html>
